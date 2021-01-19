bc72a89fa2bed1529537ee8e0dd8ebda6cc20840
protagonisths <13837050616@163.com>
2021-01-19 14:32:17 +0800

    feat: add rollup

---
+++ scripts/rollup.config.js
@@ -0,0 +1,15 @@
+import { join } from "path";
+function resolve(dir) {
+  return join(__dirname, dir);
+}
+
+export default {
+  input: resolve("../explore/index.js"),
+  output: {
+    file: resolve("../lib/bundle.js"),
+    format: "cjs",
+  },
+  watch: {
+    include: resolve("../explore/**"),
+  },
+};
